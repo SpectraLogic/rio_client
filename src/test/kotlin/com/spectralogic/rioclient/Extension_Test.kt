d6d008ffbf7195d6de67dc80d78442e9e3e3f027
david-limbach <david.limbach@spectralogic.com>
2021-12-07 14:11:59 -0700

    Space encodes to plus, but plus is interpreted in query parameters.  So manually encode plus to %20

---
+++ src/test/kotlin/com/spectralogic/rioclient/Extension_Test.kt
@@ -0,0 +1,27 @@
+/**
+ * ***************************************************************************
+ *    Copyright 2014-2021 Spectra Logic Corporation. All Rights Reserved.
+ * ***************************************************************************
+ */
+package com.spectralogic.rioclient
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+@Tag("test")
+class Extension_Test {
+
+    @Test
+    fun urlEncodeTest() {
+        val plain = "abcdefghijklmnopqrstuvwxyz 01234567890 ~!@#$%^&()_+`-={}[]:;\"'<>,.?/'"
+        val encoded = plain.urlEncode()
+        val revert = encoded.urlDecode()
+        val validCharSet = "abcdefghijklmnopqrstuvwxyz01234567890%._-ABCDEF"
+
+        assertThat(plain).isEqualTo(revert)
+        encoded.forEach {
+            assertThat(validCharSet).contains(it.toString())
+        }
+    }
+}
